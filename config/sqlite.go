097677d79530b881da550c890c17df69dc20c252
jeffersonborges <jefferson.gmb@gmail.com>
2024-06-21 17:12:06 -0300

    feat: add database handler

--- config/sqlite.go
+++ config/sqlite.go
@@ -14,33 +14,33 @@ func InitializeSQLite() (*gorm.DB, error) {
 	// Check if the database file exists
 	_, err := os.Stat(dbPath)
 	if os.IsNotExist(err) {
-		logger.Info("database file not found, creating...")
+		// logger.Info("database file not found, creating...")
 		// Create the database file and directory
 		err := os.MkdirAll("./db", os.ModePerm)
 		if err != nil {
-			logger.Errorf("database directory creation error: ", err)
+			// logger.Error(err)
 			return nil, err
 		}
 		// Create the database file
 		file, err := os.Create(dbPath)
 		if err != nil {
-			logger.Errorf("database file creation error: ", err)
+			logger.Error(err)
 			return nil, err
 		}
-		logger.Info("database file created successfully")
+		// logger.Info("database file created successfully")
 		file.Close()
 	}
 
 	// Create DB and connect
 	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 	if err != nil {
-		logger.Errorf("sqlite opening erro: ", err)
+		// logger.Errorf("sqlite opening erro: ", err)
 		return nil, err
 	}
 	// Migrate the schema
 	err = db.AutoMigrate(&schemas.Opening{})
 	if err != nil {
-		logger.Errorf("sqlite automigration error: ", err)
+		// logger.Errorf("sqlite automigration error: ", err)
 		return nil, err
 	}
 	// Return the DB
