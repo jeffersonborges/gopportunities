9d974a3d49c66b02ef89f0435c13de8bd1fb7cce
jeffersonborges <jefferson.gmb@gmail.com>
2024-06-25 21:59:53 -0300

    feat: swagger adjust basePath

--- router/routers.go
+++ router/routers.go
@@ -2,13 +2,18 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jeffersonborges/gopportunities/docs"
 	"github.com/jeffersonborges/gopportunities/handler"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize Handler
 	handler.InitializeHandler()
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHanlder)
 		v1.POST("/opening", handler.CreateOpeningHanlder)
@@ -16,4 +21,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHanlder)
 		v1.GET("/openings", handler.LitsOpeningsHanlder)
 	}
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
